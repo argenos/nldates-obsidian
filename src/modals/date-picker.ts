99102826170f7775092a84429fe364046ce40c22
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2021-04-11 15:11:36 +0200

    Add date preview to the date picker
    This commit also improves the appearance of the modal
    Fixes #24

--- src/modals/date-picker.ts
+++ src/modals/date-picker.ts
@@ -1,79 +1,176 @@
-import { App, ButtonComponent, MarkdownView, Modal, MomentFormatComponent, TextComponent, ToggleComponent } from "obsidian";
-import NaturalLanguageDates from "src/main";
-
+import {
+  App,
+  ButtonComponent,
+  MarkdownView,
+  Modal,
+  MomentFormatComponent,
+  TextAreaComponent,
+  TextComponent,
+  ToggleComponent,
+} from 'obsidian';
+import NaturalLanguageDates from 'src/main';
 
 export class ParseMomentModal extends Modal {
-  parsedDateString = "";
+  parsedDateString = '';
   activeView: MarkdownView;
   activeEditor: CodeMirror.Editor;
   activeCursor: CodeMirror.Position;
   plugin: NaturalLanguageDates;
+  inputDateField: TextComponent;
+  momentFormatField: MomentFormatComponent;
+  toggleLink: ToggleComponent;
+  dateSample: TextAreaComponent;
 
   constructor(app: App, plugin: NaturalLanguageDates) {
     super(app);
     this.plugin = plugin;
+
+    // Check if we're viewing a markdown file
     this.activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
     if (!this.activeView) return;
     this.activeEditor = this.activeView.sourceMode.cmEditor;
     this.activeCursor = this.activeEditor.getCursor();
+
+    // Add components
+    this.contentEl.createDiv('setting-item', (rowEl) => {
+      rowEl.createDiv('setting-item-info', (descEl) => {
+        descEl.createDiv({
+          cls: 'setting-item-name',
+          text: 'Date',
+        });
+
+        descEl.createDiv({
+          cls: 'setting-item-description',
+          text: 'Date in natural language',
+        });
+      });
+
+      rowEl.createDiv('setting-item-control', (ctrlEl) => {
+        this.inputDateField = new TextComponent(ctrlEl).setPlaceholder('Date');
+      });
+    });
+
+    // Moment formatting
+    this.contentEl.createDiv('setting-item', (rowEl) => {
+      rowEl.createDiv('setting-item-info', (descEl) => {
+        descEl.createDiv({
+          cls: 'setting-item-name',
+          text: 'Format',
+        });
+
+        descEl.createDiv({
+          cls: 'setting-item-description',
+          text: 'Moment format to be used',
+        });
+      });
+
+      rowEl.createDiv('setting-item-control', (ctrlEl) => {
+        this.momentFormatField = new MomentFormatComponent(ctrlEl)
+          .setDefaultFormat('YYYY-MM-DD HH:mm')
+          .setValue(this.plugin.settings.modalMomentFormat);
+      });
+    });
+
+    // Link toggle
+    this.contentEl.createDiv('setting-item', (rowEl) => {
+      rowEl.createDiv('setting-item-info', (descEl) => {
+        descEl.createDiv({
+          cls: 'setting-item-name',
+          text: 'Add as link?',
+        });
+
+        descEl.createDiv({
+          cls: 'setting-item-description',
+          text: 'Whether to add the date as a link',
+        });
+      });
+
+      rowEl.createDiv('setting-item-control', (ctrlEl) => {
+        this.toggleLink = new ToggleComponent(ctrlEl).setValue(
+          this.plugin.settings.modalToggleLink,
+        );
+      });
+    });
+
+    // Sample date
+    this.contentEl.createDiv('setting-item', (rowEl) => {
+      rowEl.createDiv('setting-item-info', (descEl) => {
+        descEl.createDiv({
+          cls: 'setting-item-name',
+          text: 'Preview',
+        });
+
+        descEl.createDiv({
+          cls: 'setting-item-description',
+          text: 'Parsed date as it will be inserted',
+        });
+      });
+      rowEl.createDiv('setting-item-control', (ctrlEl) => {
+        this.dateSample = new TextAreaComponent(ctrlEl).setValue(this.parsedDateString);
+      });
+    });
   }
 
   onOpen() {
-    let {
-      contentEl
-    } = this;
+    let { contentEl } = this;
 
-    contentEl.appendText("Date: ");
+    this.momentFormatField.onChange((value) => {
+      this.plugin.settings.modalMomentFormat = value ? value : 'YYYY-MM-DD HH:mm';
+      this.plugin.saveSettings();
+      this.updateSampleDate();
+    });
 
-    let inputDateField = new TextComponent(contentEl).setPlaceholder("Date");
-    contentEl.createEl("br");
-    contentEl.appendText("Format: ");
+    this.toggleLink.onChange((value) => {
+      this.plugin.settings.modalToggleLink = value;
+      this.plugin.saveSettings();
+      this.updateSampleDate();
+    });
 
-    let momentFormatField = new MomentFormatComponent(contentEl)
-      .setDefaultFormat("YYYY-MM-DD HH:mm")
-      .setValue(this.plugin.settings.modalMomentFormat)
-      .onChange((value) => {
-        this.plugin.settings.modalMomentFormat = value ? value : "YYYY-MM-DD HH:mm";
-        this.plugin.saveSettings();
-      });
+    this.inputDateField.onChange((value) => {
+      this.updateSampleDate();
+    });
 
-    contentEl.createEl("br");
-
-    contentEl.appendText("Add as link?");
-    let toggleLink = new ToggleComponent(contentEl)
-      .setValue(this.plugin.settings.modalToggleLink)
-      .onChange((value) => {
-        this.plugin.settings.modalToggleLink = value;
-        this.plugin.saveSettings();
-      });
-    contentEl.createEl("br");
-
-    let inputButton = new ButtonComponent(contentEl)
-      .setButtonText("Insert date")
-      .onClick(() => {
-        let parsedDate = this.plugin.parseDate(inputDateField.getValue());
-        this.parsedDateString = parsedDate.moment.format(
-          momentFormatField.getValue()
+    contentEl.createDiv('modal-button-container', (el) => {
+      new ButtonComponent(el).setButtonText('Insert date').onClick(() => {
+        this.getFormattedDate(
+          this.inputDateField.getValue(),
+          this.momentFormatField.getValue(),
+          this.toggleLink.getValue(),
         );
-        if (!parsedDate.moment.isValid()) this.parsedDateString = "";
-        if (toggleLink.getValue() && this.parsedDateString !== "")
-          this.parsedDateString = `[[${this.parsedDateString}]]`;
+
         this.activeEditor.focus();
         this.activeEditor.setCursor(this.activeCursor);
         this.plugin.insertDateString(
           this.parsedDateString,
           this.activeEditor,
-          this.activeCursor
+          this.activeCursor,
         );
         this.close();
       });
-    inputDateField.inputEl.focus();
+    });
+
+    this.inputDateField.inputEl.focus();
+  }
+
+  updateSampleDate() {
+    this.getFormattedDate(
+      this.inputDateField.getValue(),
+      this.momentFormatField.getValue(),
+      this.toggleLink.getValue(),
+    );
+    this.dateSample.setValue(this.parsedDateString);
+  }
+
+  getFormattedDate(input: string, format: string, asLink: boolean) {
+    let parsedDate = this.plugin.parseDate(input);
+    this.parsedDateString = parsedDate.moment.format(format);
+    if (!parsedDate.moment.isValid()) this.parsedDateString = '';
+    if (asLink && this.parsedDateString !== '')
+      this.parsedDateString = `[[${this.parsedDateString}]]`;
   }
 
   onClose() {
-    let {
-      contentEl
-    } = this;
+    let { contentEl } = this;
     contentEl.empty();
   }
 }
