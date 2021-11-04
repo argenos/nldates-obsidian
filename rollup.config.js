cb2bc2678b83cf21b4493740b7afb83c99b1e889
Rens Oliemans <hallo@rensoliemans.nl>
2021-11-04 14:00:07 +0100

    :pencil2: Build into . instead of build

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from "@rollup/plugin-commonjs";
 export default {
   input: "src/main.ts",
   output: {
-    dir: "build",
+    dir: ".",
     sourcemap: "inline",
     format: "cjs",
     exports: "default",
